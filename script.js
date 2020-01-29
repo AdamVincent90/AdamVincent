256630ee025c0c15fb7440c34af1997ad05cde60
AdamVincent90 <37915853+AdamVincent90@users.noreply.github.com>
2020-01-29 20:51:08 +0000

    standalone

---
+++ script.js
@@ -0,0 +1,9 @@
+/* SCRIPTS.JS */
+
+function changePic() {
+    document.getElementById("profilePic").src = "https://i.imgur.com/dVvC6CV.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "https://i.imgur.com/dVvC6CV.jpg";
+  }
